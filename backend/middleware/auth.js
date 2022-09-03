83ce2b93ced1872b8f9d154f83b002cfcfdf7d87
NonoGitCode <99494935+NonoGitCode@users.noreply.github.com>
2022-09-03 13:40:29 +0200

    modification dotenv

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-let secretToken = process.env.token
+// require('dotenv').config();
+// let secretToken = process.env.token
 
 //Extraction des informations d'authentification, vérification, puis transmition.
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[0];
-        const decodedToken = jwt.verify(token,`${secretToken}`);
+        const decodedToken = jwt.verify(token,`RANDOM_SECRET_TOKEN`);
         const userId = decodedToken.userId;
         const level = decodedToken.level
         const pseudo = decodedToken.pseudo
